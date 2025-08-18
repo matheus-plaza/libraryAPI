23f5fc58c4558e4d5615369f2870fc01a2946cb5
Matheus Plaza <plazachave.mp7@gmail.com>
2025-08-18 12:36:40 -0300

    feat(livros): Validacao campo invalido e Paginacao de livro

---
+++ src/main/java/plaza/libraryapi/exceptions/CampoInvalidoException.java
@@ -0,0 +1,7 @@
+package plaza.libraryapi.exceptions;
+
+public class CampoInvalidoException extends RuntimeException {
+  public CampoInvalidoException(String message) {
+    super(message);
+  }
+}
