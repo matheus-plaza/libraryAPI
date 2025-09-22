5e7ba506d6b0caf3e8f0723fc2c1cd5d2a257a26
Matheus Plaza <plazachave.mp7@gmail.com>
2025-09-22 10:42:12 -0300

    feat(auth): Adiciona informações customizadas no Token JWT

--- src/main/java/plaza/libraryapi/config/AuthorizationServerConfiguration.java
+++ src/main/java/plaza/libraryapi/config/AuthorizationServerConfiguration.java
@@ -11,22 +11,29 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import plaza.libraryapi.security.CustomAuthentication;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Configuration
@@ -120,4 +127,24 @@ public class AuthorizationServerConfiguration {
                 .build();
     }
 
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(){
+        return context -> {
+            var principal = context.getPrincipal();
+
+            if(principal instanceof CustomAuthentication authentication){
+                OAuth2TokenType tipoToken = context.getTokenType();
+
+                if(OAuth2TokenType.ACCESS_TOKEN.equals(tipoToken)){
+                    Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+                    var list = authorities.stream().map(GrantedAuthority::getAuthority).toList();
+
+                    context
+                            .getClaims()
+                            .claim("authorities", list)
+                            .claim("email", authentication.getUsuario().getEmail());
+                }
+            }
+        };
+    }
 }
